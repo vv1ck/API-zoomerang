6cce8101ec4f984168b7adf72b34f2bed2a0c482
vv1ck <75469718+vv1ck@users.noreply.github.com>
2021-03-26 03:45:22 +0300

    Create zoomerang-SWAP.py

---
+++ zoomerang-SWAP.py
@@ -0,0 +1,21 @@
+""" API swap ( zoomerang )"""
+
+
+urSW = 'https://us-central1-zoomerang-dcf49.cloudfunctions.net/apiv2/v2/user/username'
+
+hedSW = {
+	'accept': 'application/json',
+	'accept-encoding': 'br;q=1.0, gzip;q=0.9, deflate;q=0.8',
+	'accept-language': 'ar-JO;q=1.0, en-JO;q=0.9',
+	'authorization': 'Bearer '+sis, # << Take it after logging in
+	'content-length': '37',
+	'content-Type': 'application/json',
+	'user-agent': 'Zoomerang/3.0.16 (com.yantech.zoomerang; build:3; iOS 13.5.0) Alamofire/5.2.2'}
+
+datSW = {
+'username': user}
+
+requests.put()
+
+
+""" By JOKER @t.uo """
