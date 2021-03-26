9f11f6b5accfeaeef4ae8bc3c88483dca677638d
vv1ck <75469718+vv1ck@users.noreply.github.com>
2021-03-26 03:49:56 +0300

    Create zoomerang-Check-username.py

---
+++ zoomerang-Check-username.py
@@ -0,0 +1,24 @@
+""" API Check username ( zoomerang )"""
+
+
+url = 'https://us-central1-zoomerang-dcf49.cloudfunctions.net/apiv2/v2/user/short?username='+username
+
+headers = {
+	'accept': 'application/json, text/plain, */*',
+	'accept-language': 'en-US,en;q=0.9',
+	'accept-encoding': 'gzip, deflate, br',
+	'cache': '',
+	'if-none-match': 'W/"f8-Y4OTW9r4NK14xQ+V9hO/LQ9dyFY""',
+	'origin': 'https://zoomerang.app',
+	'referer': 'https://zoomerang.app/',
+	'sec-fetch-dest': 'empty',
+	'sec-fetch-mode': 'cors',
+	'sec-fetch-site': 'cross-site',
+	'sec-fetch-user': '?1',
+	'upgrade-insecure-requests': '1',
+	'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'}
+	
+requests.post()
+
+
+""" By JOKER @t.uo """
